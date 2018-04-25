bbee9567a812af7d35669ff517190d50df5a9b28
寇有全 <kou_youquan@163.com>
2018-04-25 10:21:03 +0800

    1、上传

---
+++ FFKnowledge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FFKnowledge
+//
+//  Created by cts on 2018/4/25.
+//  Copyright © 2018年 cts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FFKnowledge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FFKnowledge
+//
+//  Created by cts on 2018/4/25.
+//  Copyright © 2018年 cts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
